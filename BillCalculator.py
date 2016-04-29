b2f31287069c8151639b82b0ebaff3adadfd0a07
Me <me@me.com>
2016-04-28 21:09:56 -0700

    Class exercise on bill calculator

---
+++ BillCalculator.py
@@ -0,0 +1,43 @@
+bill_amount = 10
+tip_percentage = .20 
+tip = bill_amount * tip_percentage
+
+# def tip_amount(bill_amount,tip_percentage):
+# 	total = bill_amount*tip_percentage
+# 	return total
+
+# def bill(tip, bill_amount):
+# #     total_bill = tip + bill_amount
+# #     return total_bill
+
+# def split_bill(bill_amount, num_people):
+# 	total_bill = tip + bill_amount
+# 	total_split = total_bill/num_people
+# 	return total_split
+
+
+# num_people = 8
+
+# #print tip_amount(bill_amount,tip_percentage)
+# #print bill(tip, bill_amount)
+# print split_bill(bill_amount, num_people)
+
+
+def main():
+		print "Please choose one of the following."
+		print "1 - Calculate Tip"
+		print "2 - Split Bill"
+		choice = int(raw_input())
+
+	if choice = 1
+
+#if choice 1, print tip amount + bill
+    #raw_input (Would you like to have the bill split?)
+    #if "yes", raw_input (How many ways would you like to split the bill?
+        #input split bill function
+        #print result for choice 1 with split bill
+    #elif "no", print tip amount + bill because the bill is not split
+        #print result for choice 1 with zero people splitting the bill
+#if choice 2, print the cost per person based on the bill total and number of persons
+	#input split bill function
+        #print result for choice 1 with split bill
---
+++ BillCalculatorVersion2.py
@@ -0,0 +1,3 @@
+"""
+Let's create a different Python fill to practice the new skills ew learned, BillCalculatorVersion2
+"""
